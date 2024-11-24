5cd31e26060510774b8fa82b170e18eef00a41b9
andrewwashuta-8451 <andrew.washuta@gmail.com>
2024-11-24 00:41:30 -0700

    Added SCSS and cleaning up a few styles
    Fixed structure and updated favicon, along with a few other changes. Added variables / global SCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://andrewwashuta.github.io',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+            api: 'modern-compiler' // or "modern"
+        },
+      },
+    },
+  },
+});
+  
