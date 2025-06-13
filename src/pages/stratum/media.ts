a3dfecca1a6de66b50e0344a0c7067fd53ac19f6
andrewwashuta-8451 <andrew.washuta@gmail.com>
2025-06-13 01:02:12 -0600

    fix: correct workshop image extension case

--- src/pages/stratum/media.ts
+++ src/pages/stratum/media.ts
@@ -2,7 +2,7 @@ import stratumHeroImg from '/src/assets/index/images/stratum_thumb.png';
 import stratumHeroVideo from "/src/assets/projects/stratum/videos/stratum_ai.mp4";
 import quickLauncher from "/src/assets/projects/stratum/images/quick_launcher.png";
 import progressive from "/src/assets/projects/stratum/videos/progressive-disclosure.mp4";
-import workshop from "/src/assets/projects/stratum/images/workshop.jpg";
+import workshop from "/src/assets/projects/stratum/images/workshop.JPG";
 
 export const stratumMedia = {
   hero: {
