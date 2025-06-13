d78b94a4a4bf9ea45ba2440fce7e61ee767d0cdb
andrewwashuta-8451 <andrew.washuta@gmail.com>
2025-06-13 01:16:22 -0600

    Pushed some fixes, deprecated gallery

--- src/pages/stratum/media.ts
+++ src/pages/stratum/media.ts
@@ -2,7 +2,7 @@ import stratumHeroImg from '/src/assets/index/images/stratum_thumb.png';
 import stratumHeroVideo from "/src/assets/projects/stratum/videos/stratum_ai.mp4";
 import quickLauncher from "/src/assets/projects/stratum/images/quick_launcher.png";
 import progressive from "/src/assets/projects/stratum/videos/progressive-disclosure.mp4";
-import workshop from "/src/assets/projects/stratum/images/workshop.JPG";
+import workshop from "/src/assets/projects/stratum/images/workshop.jpg";
 
 export const stratumMedia = {
   hero: {
@@ -10,12 +10,12 @@ export const stratumMedia = {
     video: stratumHeroVideo,
   },
   quickLauncher: {
-    image: quickLauncher
+    image: quickLauncher,
   },
   progressive: {
-    video: progressive
+    video: progressive,
   },
   workshop: {
-    image: workshop
-  }
+    image: workshop,
+  },
 } as const; 
