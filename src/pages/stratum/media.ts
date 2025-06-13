b45d098c5d222413883dd35def454d6875d34fa6
andrewwashuta-8451 <andrew.washuta@gmail.com>
2025-06-13 00:53:39 -0600

    Image improvements, styling, updating copy on Stratum
    Improved title structure, as well across pages. Really just improved a lot. Need to revisit more of Stratum tomorrow. I'm hoping to wrap that up.

--- src/pages/stratum/media.ts
+++ src/pages/stratum/media.ts
@@ -2,6 +2,7 @@ import stratumHeroImg from '/src/assets/index/images/stratum_thumb.png';
 import stratumHeroVideo from "/src/assets/projects/stratum/videos/stratum_ai.mp4";
 import quickLauncher from "/src/assets/projects/stratum/images/quick_launcher.png";
 import progressive from "/src/assets/projects/stratum/videos/progressive-disclosure.mp4";
+import workshop from "/src/assets/projects/stratum/images/workshop.jpg";
 
 export const stratumMedia = {
   hero: {
@@ -9,9 +10,12 @@ export const stratumMedia = {
     video: stratumHeroVideo,
   },
   quickLauncher: {
-    image: quickLauncher,
+    image: quickLauncher
   },
   progressive: {
-    video: progressive,
+    video: progressive
   },
+  workshop: {
+    image: workshop
+  }
 } as const; 
