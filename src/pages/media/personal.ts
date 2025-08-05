1d6c53865a4f349c3ed70a6957a8f127f6fc9ad9
andrewwashuta-8451 <andrew.washuta@gmail.com>
2025-08-04 23:43:29 -0600

    More content added to personal
    Also, updated projectmedia to leave off margin on the last child

--- src/pages/media/personal.ts
+++ src/pages/media/personal.ts
@@ -11,6 +11,9 @@ import stag from '../../assets/projects/personal/images/stag.png';
 import electrobinoculars from '../../assets/projects/personal/images/hoth-macrobinoculars.png';
 import flyby from '../../assets/projects/personal/images/star-wars-flyby.png';
 import nixie from '../../assets/projects/personal/images/nixie-numerals.png';
+import thorMando from '../../assets/projects/personal/images/thor-mando.jpg';
+import mandoShirt from '../../assets/projects/personal/images/mando-shirt.png';
+import valentine2024 from '../../assets/projects/personal/images/valentines-2024.png';
 
 export const personalMedia = {
   holiday2017: {
@@ -51,5 +54,14 @@ export const personalMedia = {
   },
   nixie: {
     image: nixie,
+  },
+  thorMando: {
+    image: thorMando,
+  },
+  mandoShirt: {
+    image: mandoShirt,
+  },
+  valentine2024: {
+    image: valentine2024,
   }
 } as const; 
