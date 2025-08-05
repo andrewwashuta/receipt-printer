4ef2c05e7800c4f16feb897ba6918337ea9ca6bd
andrewwashuta-8451 <andrew.washuta@gmail.com>
2025-08-04 18:09:40 -0600

    Tons of updates to my personal section
    Added in the holiday card / Star Wars explorations, as well as improved some minor things on Meridian

---
+++ src/pages/media/personal.ts
@@ -0,0 +1,55 @@
+import holiday2017 from '../../assets/projects/personal/images/holiday-2017.png';
+import holiday2018 from '../../assets/projects/personal/images/holiday-2018.png';
+import holiday2019 from '../../assets/projects/personal/images/holiday-2019.png';
+import holiday2020 from '../../assets/projects/personal/images/holiday-2020.png';
+import holiday2021 from '../../assets/projects/personal/images/holiday-2021.png';
+import holiday2022 from '../../assets/projects/personal/images/holiday-2022.png';
+import holiday2023 from '../../assets/projects/personal/images/holiday-2023.png';
+import holiday2024 from '../../assets/projects/personal/images/holiday-2024.png';
+import holidayCards from '../../assets/projects/personal/images/holiday-cards.png';
+import stag from '../../assets/projects/personal/images/stag.png';
+import electrobinoculars from '../../assets/projects/personal/images/hoth-macrobinoculars.png';
+import flyby from '../../assets/projects/personal/images/star-wars-flyby.png';
+import nixie from '../../assets/projects/personal/images/nixie-numerals.png';
+
+export const personalMedia = {
+  holiday2017: {
+    image: holiday2017,
+  },
+  holiday2018: {
+    image: holiday2018,
+  },
+  holiday2019: {
+    image: holiday2019,
+  },
+  holiday2020: {
+    image: holiday2020,
+  },
+  holiday2021: {
+    image: holiday2021,
+  },
+  holiday2022: {
+    image: holiday2022,
+  },
+  holiday2023: {
+    image: holiday2023,
+  },
+  holiday2024: {
+    image: holiday2024,
+  },
+  holidayCards: {
+    image: holidayCards,
+  },
+  stag: {
+    image: stag,
+  },
+  electrobinoculars: {
+    image: electrobinoculars,
+  },
+  flyby: {
+    image: flyby,
+  },
+  nixie: {
+    image: nixie,
+  }
+} as const; 
