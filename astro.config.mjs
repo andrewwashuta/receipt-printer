5d5ac444ae19e8edf7ea510074d466da4348efeb
andrewwashuta-8451 <andrew.washuta@gmail.com>
2025-08-13 14:18:09 -0600

    Updated site link in the astor config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
-  site: 'https://andrewwashuta.github.io',
+  site: 'https://andrew.cv',
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp'
